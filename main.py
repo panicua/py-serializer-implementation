fb877696c587439c54ff414396a867b439fbc4c8
kekW <panicua12@gmail.com>
2024-05-25 17:50:22 +0300

    feat: main.py/ serialization and deserialization

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(instance=car)
+    json = JSONRenderer().render(serializer.data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    serializer_data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=serializer_data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return serializer.instance
+    else:
+        return serializer.errors
